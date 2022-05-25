e587ccf5b51447cbb8fc3270165f32dece320b0d
Nestle <leuninestor@gmail.com>
2022-05-25 15:34:05 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# terraform
+Terraform and AWS codes
---
+++ main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.region
+  profile = var.profile_name
+}
+
+# Create a VPC
+resource "aws_vpc" "app_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "app-vpc"
+  }
+}
+
+# internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  tags = {
+    Name = "vpc_igw"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id            = aws_vpc.app_vpc.id
+  cidr_block        = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public_rt"
+  }
+}
+
+# Associate Subnet Route Table
+resource "aws_route_table_association" "public_rt_asso" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# Create ubuntu Server and install/enable apache
+resource "aws_instance" "web" {
+  ami           = "ami-04505e74c0741db8d" 
+  instance_type = var.instance_type
+  key_name = var.instance_key
+  subnet_id              = aws_subnet.public_subnet.id
+  security_groups = [aws_security_group.sg.id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "*** Installing apache2"
+  sudo apt update -y
+  sudo apt install apache2 -y
+  echo "*** Completed Installing apache2"
+  EOF
+
+  tags = {
+    Name = "web_instance"
+  }
+
+  # volume_tags = {
+  #   Name = "web_instance"
+  # } 
+}
